f0d40fcb32d66574059a213046b8ba383ce5f675
Z4NO-V2 <92402266+baloncesto2005@users.noreply.github.com>
2024-03-15 09:35:52 +0100

    second commit

---
+++ src/Modelos/Model/copy/Persona.java
@@ -0,0 +1,67 @@
+package Modelos.Model.copy;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String Nombre;
+
+    /**
+     * 
+     */
+    private String Apellido1;
+
+    /**
+     * 
+     */
+    private String Apellido2;
+
+    /**
+     * 
+     */
+    private int EDAD = 0;
+
+    /**
+     * @param Nombre 
+     * @param Apellido1 
+     * @param Apellido2 
+     * @param dni 
+     * @param EDAD
+     */
+    public void   (String Nombre, String Apellido1, String Apellido2, String dni, int EDAD) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void SetDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String GetDni() {
+        // TODO implement here
+        return "";
+    }
+
+}
