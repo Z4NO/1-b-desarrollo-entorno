111f5dca44a41ab6fe4c3277be8d70f06f76bcce
Z4NO-V2 <92402266+Z4NO@users.noreply.github.com>
2024-03-21 10:23:48 +0100

    casa update

---
+++ Persona/src/Modelos/Model/copy/casa.java
@@ -0,0 +1,78 @@
+package Modelos.Model.copy;
+
+import java.util.*;
+
+public class casa {
+	 /**
+     * Default constructor
+     */
+
+    /**
+     * 
+     */
+    private int String;
+
+    /**
+     * 
+     */
+    private int numHabitaciones;
+
+    /**
+     * 
+     */
+    public int metrosCuadrados;
+
+    /**
+     * @param direccion 
+     * @param numHabitaciones 
+     * @param metrosCuadrados
+     */
+    public void Casa(String direccion, int numHabitaciones, int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDireccion() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getNumHabitaciones() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param direccion
+     */
+    public void setDireccion(String direccion) {
+        // TODO implement here
+    }
+
+    /**
+     * @param numHabitaciones
+     */
+    public void setNumHabitaciones(int numHabitaciones) {
+        // TODO implement here
+    }
+
+    /**
+     * @param metrosCuadrados
+     */
+    public void setmetrosCuadrados(int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String toString() {
+        // TODO implement here
+        return "";
+    }
+}
